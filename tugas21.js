7d212cae2b8a65dac858034f21e80434ed50a244
Ignasius Yuda Adhitia <ignasius.yuda.adhitia@gmail.com>
2021-12-22 07:18:46 +0700

    Pengumpulan Tugas 21 JS - NIOMIC

---
+++ tugas21.js
@@ -0,0 +1,8 @@
+console.log("REPLACE\n=======================");
+
+let teks = "Saya belajar masak nasi goreng";
+console.log(teks);
+let teksReplace = teks.replace("masak", "makan");
+console.log(teksReplace);
+// atau
+console.log(teks.replace("masak", "makan"));
